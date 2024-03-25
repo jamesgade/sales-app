ea47b803a008530e2d1992b61b3a24ef95ff07ad
Bandana Banerjee <bbandana2012@gmail.com>
2024-03-25 20:36:45 +0530

    not found

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div>
+            <h1>Oops</h1>
+            <p>404 - Page not found !</p>
+            <Link to='/'>
+                <FaHome /> Back To Home
+            </Link>
+        </div>
     )
 }
 
