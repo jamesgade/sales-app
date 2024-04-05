6970a0d9a4a47156302da6ddb3ef4a7642e25783
Bandana Banerjee <bbandana2012@gmail.com>
2024-04-05 21:33:07 +0530

    fixed css

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -3,12 +3,15 @@ import { FaHome } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 const NotFound = () => {
     return (
-        <div>
-            <h1>Oops</h1>
-            <p>404 - Page not found !</p>
-            <Link to='/'>
-                <FaHome /> Back To Home
-            </Link>
+        <div className='notfound'>
+            <div className="notfound-wrapper">
+
+                <h1>Oops</h1>
+                <p>404 - Page not found !</p>
+                <Link to='/'>
+                    <FaHome /> Back To Home
+                </Link>
+            </div>
         </div>
     )
 }
